b5ef05826ae100895a0eb5996c6aefc0a4653736
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-03-20 04:06:59 +0530

    c

---
+++ gpt3_jsm/src/components/navbar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './navbar.css';
+
+const NavBar = () => {
+  return (
+    <div><h1>NavBar</h1></div>
+  )
+}
+
+export default NavBar
---
+++ gpt3_jsm/src/containers/possibility/Posibility.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './possibility.css'
+
+const Posibility = () => {
+  return (
+    <div><h1>Posibility</h1></div>
+  )
+}
+
+export default Posibility
